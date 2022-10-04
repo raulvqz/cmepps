951c2da8110376848c6f84ec4cfb74e1b30d67ab
usuario <usuario@pc22-039>
2022-10-04 10:55:03 +0200

    Añadido saludo

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -4,6 +4,10 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		
+		String name="Raul";
+		System.out.println("Bienvenidos al curso"+name);
+		
 
 	}
 
